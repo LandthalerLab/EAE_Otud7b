193f0c402075b425975f72b54862193a15b036bb
emanuelwyler <emanuel.wyler@mdc-berlin.de>
2025-06-26 16:52:44 +0200

    Add files via upload

--- Step1_read_merge_data.R
+++ Step1_read_merge_data.R
@@ -2,39 +2,44 @@
 
 options(rgl.useNULL=TRUE)
 
-#Install Voltron from github
-devtools::install_github("Artur-man/VoltRon")
+#Install Voltron from github if not done previously
+#devtools::install_github("Artur-man/VoltRon")
 
 library(Seurat)
 library(VoltRon)
+library(rJava)
+library(RBioFormats)
+library(arrow)
 
+working_dir <- "/path/to/working/directory/"
+data_dir <- "/path/to/data/dir/"
 
-setwd("/path/to/working/directory/")
+setwd(working_dir)
 
 #Read all Xenium output folders as they come from the machine
-d0_WT1 <- importXenium("d0_WT1/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_WT1 <- importXenium(paste0(data_dir, "d0_WT1/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d0_WT2 <- importXenium("d0_WT2/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_WT2 <- importXenium(paste0(data_dir, "d0_WT2/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d0_WT3 <- importXenium("d0_WT3/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_WT3 <- importXenium(paste0(data_dir, "d0_WT3/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d0_KO1_slice1 <- importXenium("d0_KO1_slice1/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_KO1_slice1 <- importXenium(paste0(data_dir, "d0_KO1_slice1/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d0_KO2_slice2 <- importXenium("d0_KO2_slice2/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_KO2_slice2 <- importXenium(paste0(data_dir, "d0_KO2_slice2/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d0_KO3 <- importXenium("d0_KO3/outs/", resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
+d0_KO3 <- importXenium(paste0(data_dir, "d0_KO3/"), resolution_level = 3, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_WT1 <- importXenium("d15_WT1/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_WT1 <- importXenium(paste0(data_dir, "d15_WT1/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_WT2 <- importXenium("d15_WT2/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_WT2 <- importXenium(paste0(data_dir, "d15_WT2/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_WT3 <- importXenium("d15_WT3/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_WT3 <- importXenium(paste0(data_dir, "d15_WT3/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_KO1 <- importXenium("d15_KO1/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_KO1 <- importXenium(paste0(data_dir, "d15_KO1/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_KO2 <- importXenium("d15_KO2/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_KO2 <- importXenium(paste0(data_dir, "d15_KO2/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
-d15_KO3_slice2 <- importXenium("d15_KO3_slice2/outs/", resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
+d15_KO3_slice2 <- importXenium(paste0(data_dir, "d15_KO3_slice2/"), resolution_level = 2, overwrite_resolution = TRUE, import_molecules = TRUE)
 
 #merge day 0 and d15 objects
 expr <- list(d0_WT1, d0_WT2, d0_WT3, d0_KO1_slice1, d0_KO2_slice2, d0_KO3)
